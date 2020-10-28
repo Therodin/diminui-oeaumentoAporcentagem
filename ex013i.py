57137a8be91522078437026f42cd92914863b546
Therodin <73198273+Therodin@users.noreply.github.com>
2020-10-28 17:49:40 -0300

    Add files via upload

---
+++ ex013i.py
@@ -0,0 +1,17 @@
+i = str(input('Deseja caucular um aumento ou um abaixo salarial?: '))
+
+
+if i == 'aumento':
+    am = float(input('Qual o salário do funcionario? R$: '))
+    pc = float(input('Qual a porcentagem do reajuste?: '))
+
+    ea = am + (am * pc / 100)
+    print(f'O aumento em {pc:.0f}% salarial do funcionario ficará {ea:.2f}' )
+
+else:
+      i = 'abaixo'
+      ab = float(input('Qual a porcentagem do reajuste?: '))
+      pct = float(input('Qual a porcentagem do reajuste?: '))
+
+      ae = ab - (ab * pct / 100)
+      print(f'O desconto salarial avaliado em {pct:.0f}% do funcionário será {ae:.2f}')
